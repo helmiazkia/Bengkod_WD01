ccb0b61f966ad8812a9ae78325b36a4c67829fdc
Helmi azkia <azkiasekian@gmail.com>
2025-03-24 15:03:19 +0700

    migrasi

--- database/migrations/2025_03_24_075512_create_detail_periksas_table.php
+++ database/migrations/2025_03_24_075512_create_detail_periksas_table.php
@@ -4,22 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('detail_periksas', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('id_periksa')->constrained('periksas')->onDelete('cascade');
+            $table->foreignId('id_obat')->constrained('obats')->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('detail_periksas');
